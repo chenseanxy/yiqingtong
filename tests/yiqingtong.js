a65ae5c1138bab994d0672fdce4f73b4622e5e43
Chen Xinyang <chenxy1999728@126.com>
2020-07-13 14:54:54 +0800

    Added comments

--- tests/yiqingtong.js
+++ tests/yiqingtong.js
@@ -5,18 +5,26 @@ module.exports = {
         browser
             .init()
             .waitForElementVisible('body', 1000)
+            
+            // Login Page 
             .setValue('input[type=text]', config.username)
             .pause(100)
             .setValue('input[type=password]', config.password)
             .pause(100)
             .click('div.btn')
             .waitForElementVisible('body', 1000)
+
+            // Main Page
+            // Geolocation
             .click('div[name=area] > input[readonly=readonly]')
             .waitForElementNotVisible('div.page-loading-container', 2000)
             .pause(100)
+
+            // Submit
             .click('div.footers > a')
             .waitForElementVisible('div.wapcf-inner', 500)
             .pause(100)
+            // Confirm
             .click('div.wapcf-btn-ok')
             .waitForElementVisible('div.wapat-inner', 500)
             .end()
