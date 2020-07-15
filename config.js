7fa86d63090945dd7e32d397a7f6d00b97a17638
Xinyang Chen <chenxy1999728@126.com>
2020-07-16 01:12:45 +0800

    Added error checking

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 
+const username = process.env.NW_USERNAME;
+const password = process.env.NW_PASSWORD;
+
+if(username === "" || password === ""){
+    throw Error("Username or password not configured");
+}
+
 module.exports = {
-    username: process.env.NW_USERNAME,
-    password: process.env.NW_PASSWORD,
+    username, password
 };
